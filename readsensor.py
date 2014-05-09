240c01695f197a58cb22f72508a519dc4d7ca0de
David Huntley <dhuntley1023@hotmail.com>
2014-05-08 23:15:56 -0700

    Added PSI and Temperature output to readsensor.py

--- readsensor.py
+++ readsensor.py
@@ -7,11 +7,15 @@ iodir_register = 0x01
 while True:
 	with i2c.I2CMaster() as bus:
 		bytes = bus.transaction(
-			i2c.reading(address, 2)
+			i2c.reading(address, 3)
 			)
 	
+	pressure_val = (bytes[0][0] << 8) + bytes[0][1]
+	print("PSI DIFF =", (pressure_val - 1667) * 5 /(14745 - 1667))
 	
-	output_val = (bytes[0][0] << 8) + bytes[0][1]
-	print(output_val)
+	temp_val = bytes[0][2]
+	print("TEMP (C) =", ((temp_val << 3) / 2047 * 200) - 50)
+	
+	print()
 	
 	time.sleep(1)
