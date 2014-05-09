b9ffec0487783abce9b21a50b995b12582bd1de0
David Huntley <dhuntley1023@hotmail.com>
2014-05-09 10:35:04 -0700

    Used 4th data byte from sensor to increase temp resolution

--- readsensor.py
+++ readsensor.py
@@ -7,14 +7,14 @@ iodir_register = 0x01
 while True:
 	with i2c.I2CMaster() as bus:
 		bytes = bus.transaction(
-			i2c.reading(address, 3)
+			i2c.reading(address, 4)
 			)
 	
 	pressure_val = (bytes[0][0] << 8) + bytes[0][1]
 	print("PSI DIFF = %.2f" % ((pressure_val - 1667) * 5 /(14745 - 1667)))
 	
-	temp_val = bytes[0][2]
-	print("TEMP (C) = %.2f" % (((temp_val << 3) / 2047 * 200) - 50))
+	temp_val = (bytes[0][2] << 3) + (bytes[0][3] >> 5)
+	print("TEMP (C) = %.2f" % ((temp_val / 2047 * 200) - 50))
 	
 	print()
 	
