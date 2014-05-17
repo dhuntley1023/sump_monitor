85219615dbcf6350e8db833026f4a02fbc80f99c
David Huntley <dhuntley1023@hotmail.com>
2014-05-16 22:16:36 -0700

    t.py: Now catching timeout excption

--- t.py
+++ t.py
@@ -5,9 +5,6 @@ import sqlite3
 address = 0x38
 iodir_register = 0x01
 
-conn = sqlite3.connect('temp.db', 15) #Increased timeout to 15s as temporary workaround for long-selects
-c = conn.cursor()
-
 while True:
 	with i2c.I2CMaster() as bus:
 		bytes = bus.transaction(
@@ -19,11 +16,18 @@ while True:
 	temp_val = temp_highbyte + temp_lowbyte
 
 	temperature = (((temp_val / 2047 * 200) - 50) * 9/5 + 32)
-#	print(temperature)
 
-# // print("%.3f, %d, %d" % (temperature, temp_highbyte, temp_lowbyte))
-
-	c.execute("insert into temperature values (datetime('now', 'localtime'), ?);", [temperature])
-	conn.commit()
+	try:
+		with sqlite3.connect('temp.db', 10) as db:
+			db.execute("insert into temperature values (datetime('now', 'localtime'), ?);", [temperature])
+			db.commit()
+		
+	except sqlite3.OperationalError:  
+		# print("Timed out!")
+		pass	# We want to catch the error and just move on.  We'll skip this one.
+			
+	else:
+		# print("Insert ok")
+		pass
 
 	time.sleep(15)
