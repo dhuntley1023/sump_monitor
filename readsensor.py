80e05fbe6372f3ab0078d06894ce7ea41dfc5dab
David Huntley <dhuntley1023@hotmail.com>
2014-05-30 21:41:44 -0700

    readsensor.py: Updated to use sensor module

--- readsensor.py
+++ readsensor.py
@@ -1,20 +1,14 @@
-import quick2wire.i2c as i2c
+from sensor import Sensor
 import time
 
 address = 0x38
 iodir_register = 0x01
 
 while True:
-	with i2c.I2CMaster() as bus:
-		bytes = bus.transaction(
-			i2c.reading(address, 4)
-			)
+	(pressure, temp) = Sensor().read_pressure_and_temp()
+	print("PSI DIFF = %.4f" % (pressure))
 	
-	pressure_val = (bytes[0][0] << 8) + bytes[0][1]
-	print("PSI DIFF = %.2f" % ((pressure_val - 1667) * 5 /(14745 - 1667)))
-	
-	temp_val = (bytes[0][2] << 3) + (bytes[0][3] >> 5)
-	print("TEMP (C) = %.2f" % ((temp_val / 2047 * 200) - 50))
+	print("TEMP (F) = %.2f" % (temp))
 	
 	print()
 	
