98fa9644a8e4430b9cae68eb2aee51c77c959f00
David Huntley <dhuntley1023@hotmail.com>
2014-06-07 10:26:54 -0700

    readsensory.py: Added sensor calibration

--- readsensor.py
+++ readsensor.py
@@ -1,15 +1,19 @@
-from sensor import Sensor
+from sensor import Sensor, SensorError, PressureInH2O
 import time
 
-address = 0x38
-iodir_register = 0x01
+s = Sensor()
+print("Calibrating sensor...")
+s.calibrate_pressure()
 
+print("Reading...")
 while True:
-	(pressure, temp) = Sensor().read_pressure_and_temp()
-	print("PSI DIFF = %.4f" % (pressure))
-	
-	print("TEMP (F) = %.2f" % (temp))
-	
-	print()
-	
-	time.sleep(1)
+	try:
+		(pressure, temp) = s.read_pressure_and_temperature(p_unit = PressureInH2O)
+		print("PSI DIFF = %.2f" % (pressure))
+		print("TEMP (F) = %.2f" % (temp))
+		print()
+
+	except SensorError as e:
+		print("Exception reading sensor: ", e)
+
+	time.sleep(2)
