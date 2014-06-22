76e58490a69a4a7b03fe3bbaef1caaac228fb961
David Huntley <dhuntley1023@hotmail.com>
2014-06-22 16:48:54 -0700

    p.py: Now creating per-minute reading by averaging 60 one-sec samples

--- p.py
+++ p.py
@@ -5,12 +5,28 @@ import sqlite3
 
 s = sensor.Sensor()
 
+samples = []
+sum = 0.0
+count = 0
+
 while True:
-	depth = s.read_pressure(sensor.PressureInH2O)
+	for i in range(60):
+		depth = s.read_pressure(sensor.PressureInH2O)
+		samples.append(depth)
+		sum = sum + depth
+		count = count + 1
+		if count > 60:
+			sum = sum - samples.pop(0)
+			count = count - 1
+		# print(depth, sum, count)
+		time.sleep(1)
+
+	depth_avg = sum / count
 
 	try:
+		# print(depth_avg)
 		with sqlite3.connect('sump.db', 10) as db:
-			db.execute("insert into samples values (strftime('%Y-%m-%d %H:%M', 'now', 'localtime'), ?);", [depth])
+			db.execute("insert into samples values (strftime('%Y-%m-%d %H:%M', 'now', 'localtime'), ?);", [depth_avg])
 			db.commit()
 		
 	except sqlite3.OperationalError:  
@@ -20,5 +36,3 @@ while True:
 	else:
 		# print("Insert ok")
 		pass
-
-	time.sleep(60)
