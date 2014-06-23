b9a6da4bbd426adb3e7a074f22b6c73f90205cfd
David Huntley <dhuntley1023@hotmail.com>
2014-06-23 11:05:31 -0700

    p.py: Now sends text message when pump activates

--- p.py
+++ p.py
@@ -1,6 +1,20 @@
 import sensor
 import time
 import sqlite3
+from twilio.rest import TwilioRestClient
+
+
+def send_text_msg(m):
+	ACCOUNT_SID = "AC9079652eae20b65630a4f3b5002f8aca" 
+	AUTH_TOKEN = "b143c5168050ea776e3b60614331b89d" 
+ 
+	client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN) 
+ 
+	client.messages.create(
+		to="408-832-4061", 
+		from_="+16692216493", 
+		body=m  
+	)
 
 
 s = sensor.Sensor()
@@ -8,6 +22,9 @@ s = sensor.Sensor()
 samples = []
 sum = 0.0
 count = 0
+
+last_minute_depth = -100.0
+pump_active = False
 
 while True:
 	for i in range(60):
@@ -22,6 +39,8 @@ while True:
 		time.sleep(1)
 
 	depth_avg = sum / count
+	#print("Sending msg: depth = " + str(depth_avg));
+	#send_text_msg("Current depth: " + str(depth_avg));
 
 	try:
 		# print(depth_avg)
@@ -36,3 +55,16 @@ while True:
 	else:
 		# print("Insert ok")
 		pass
+
+
+	depth_diff = depth_avg - last_minute_depth
+	last_minute_depth = depth_avg
+
+	if (depth_diff <= -0.20) and (not pump_active):
+		send_text_msg("Sump Pump Activated...");
+		pump_active = True
+
+	if (depth_diff >= 0.0) and pump_active:
+		#send_text_msg("Sump Pump Turned Off");
+		pump_active = False
+
